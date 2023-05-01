2fd9d7c747eb3a96f08be51cd169ec1b3eb16423
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-05-01 15:45:23 +0100

    Flutter Containers

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -6,19 +6,31 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      //appBar: AppBar(title: Text("data").toUpperCase()),
-      // backgroundColor: Colors.blueGrey,
-      body: Center(
-        child: IconButton(
-          onPressed: getNumber,
-          icon: Center(
-            child: Icon(
-                Icons.alt_route_rounded,
-                size: 100,
-                color: Colors.red
-    ),
-          ),
+    return Scaffold(
+      body: Container(
+        width: 350.0,
+        height: 250.0,
+        padding: EdgeInsets.all(20.0),
+        margin: EdgeInsets.all(20.0),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            color: Colors.blueGrey,
+          //borderRadius: BorderRadius.circular(10.0),
+          border: Border.all(color: Colors.red, width: 6.0),
+          shape: BoxShape.circle,
+            image: const DecorationImage(image: AssetImage("images/lady.jpg")),
+          boxShadow: const [
+            BoxShadow(color: Colors.lightBlueAccent,
+            blurRadius: 7,
+            spreadRadius: 5,
+            offset: Offset(4,4))
+          ]
+        ),
+        child: Text(
+          "Boring",
+          style: TextStyle(fontSize: 40.0,
+              fontWeight: FontWeight.bold,
+          color: Colors.white),
         ),
       )
     );
