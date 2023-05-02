62ebef65d907de1fb3d498c05f99d31449a7d9aa
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-05-02 08:58:39 +0100

    Navigation Drawer

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -11,6 +11,33 @@ class Home extends StatelessWidget {
         title: Text("Rows and Colums"),
         centerTitle: true,
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text("Mohammed Amin Ibrahim"),
+              accountEmail: Text("mohammedaminibrahim10@gmail.com"),
+              currentAccountPicture: CircleAvatar(foregroundImage: AssetImage("images/lady.jpg"),),
+              otherAccountsPictures: [
+                CircleAvatar(foregroundImage: AssetImage("images/lady.jpg"),),
+                CircleAvatar(foregroundImage: AssetImage("images/lady.jpg"),),
+                CircleAvatar(foregroundImage: AssetImage("images/lady.jpg"),)
+              ],
+            ),
+            ListTile(leading: Icon(Icons.home), title: Text("Home"), onTap: emptyFucn,),
+            ListTile(leading: Icon(Icons.shopping_cart), title: Text("Cart"), onTap: emptyFucn,),
+            ListTile(leading: Icon(Icons.favorite), title: Text("Favorite"), onTap: emptyFucn,),
+            Padding(
+              padding: EdgeInsets.all(14.0),
+              child: Text("Lable"),
+            ),
+            ListTile(leading: Icon(Icons.label), title: Text("Red"), onTap: emptyFucn,),
+            ListTile(leading: Icon(Icons.label), title: Text("Blue"), onTap: emptyFucn,),
+            ListTile(leading: Icon(Icons.label), title: Text("Green"), onTap: emptyFucn,)
+
+          ],
+        ),
+      ),
       body: Container(
         child: ListView.builder(
             itemBuilder: (context, index){
